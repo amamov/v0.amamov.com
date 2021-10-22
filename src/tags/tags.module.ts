435dbcebaa1c10d9d2821c592bda33f7da053a28
amamov <amamov@kakao.com>
2021-10-22 13:20:55 +0900

    Refactor : #1

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { BlogsModule } from 'src/blogs/blogs.module'
+import { TagsController } from './tags.controller'
 import { TagEntity } from './tags.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TagEntity])],
+  imports: [
+    TypeOrmModule.forFeature([TagEntity]),
+    forwardRef(() => BlogsModule),
+  ],
   providers: [],
-  controllers: [],
+  controllers: [TagsController],
   exports: [TypeOrmModule.forFeature([TagEntity])],
 })
 export class TagsModule {}
