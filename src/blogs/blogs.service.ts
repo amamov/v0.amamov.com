c6d9c805f4e4447f4732c526d5b05276e02fa265
amamov <amamov@kakao.com>
2021-11-01 01:46:11 +0900

    #20

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,15 +1,116 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { Connection, FindConditions, Repository } from 'typeorm'
 import { BlogEntity } from './blogs.entity'
 import { paginate, IPaginationOptions } from 'nestjs-typeorm-paginate'
+import { UsersService } from 'src/users/users.service'
+import { BlogUploadDTO } from './dtos/blog-upload.dto'
+import { TagEntity } from 'src/tags/tags.entity'
 
 @Injectable()
 export class BlogsService {
   constructor(
     @InjectRepository(BlogEntity)
     private readonly blogsRepository: Repository<BlogEntity>,
+    private readonly ormConnection: Connection,
+    private readonly usersServie: UsersService,
   ) {}
+
+  async uploadBlogPoster(
+    authorId: string,
+    findConditions: FindConditions<BlogEntity>,
+    uploadData: BlogUploadDTO,
+    updateOption?: {
+      isNew: boolean
+      updateBlogId: string
+    },
+  ) {
+    let isNew = true
+    if (updateOption && !updateOption.isNew) {
+      isNew = false
+    }
+    const queryRunner = this.ormConnection.createQueryRunner()
+    await queryRunner.connect()
+    await queryRunner.startTransaction()
+    const author = await this.usersServie.findUserById(authorId)
+    try {
+      // Typeorm.feature([BlogEntity])의 커넥션으로 연결, 트랜젝션을 위한 queryRunner의 커넥션으로는 연결 안됨
+      // const blog = await this.blogsRepository.findOne({ isTemporary: true })
+      const blog = await queryRunner.manager
+        .getRepository(BlogEntity)
+        .findOne(findConditions)
+      if (
+        isNew &&
+        (await queryRunner.manager
+          .getRepository(BlogEntity)
+          .findOne({ title: uploadData.title }))
+      ) {
+        throw new BadRequestException(
+          '해당하는 제목의 게시물은 이미 존재합니다.',
+        )
+      }
+      blog.isTemporary = false
+      blog.title = uploadData.title
+      blog.contents = uploadData.contents
+      blog.description = uploadData.description
+      blog.isPrivate = uploadData.isPrivate as boolean
+      blog.author = author
+      // [썸네일 업로드 X]
+      // const { key: thumbnail } = await this.awsService.uploadFileToS3(
+      //   `blog/${blog.id}`,
+      //   thumbnailFile,
+      // )
+      // blog.thumbnail = thumbnail
+      const slug = uploadData.title
+        .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+        .split(' ')
+        .filter((w) => w !== '')
+        .join('-')
+
+      const checkedBlogForSlug = await queryRunner.manager
+        .getRepository(BlogEntity)
+        .findOne({ slug })
+      if (isNew) {
+        if (checkedBlogForSlug)
+          blog.slug = `${slug}-${Math.floor(Math.random() * 1000)}`
+        else blog.slug = slug
+      } else {
+        if (
+          checkedBlogForSlug &&
+          checkedBlogForSlug.id !== updateOption.updateBlogId
+        )
+          blog.slug = `${slug}-${Math.floor(Math.random() * 1000)}`
+        else blog.slug = slug
+      }
+      const tagList = uploadData.tags
+        .replace(/[`~!@$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+        .replace(/ /g, '')
+        .split('#')
+        .filter((w) => w !== '')
+      const tags: TagEntity[] = []
+      for (const tagName of tagList) {
+        const existedTag = await queryRunner.manager
+          .getRepository(TagEntity)
+          .findOne({ name: tagName })
+        if (existedTag) {
+          tags.push(existedTag)
+        } else {
+          const newTag = queryRunner.manager
+            .getRepository(TagEntity)
+            .create({ name: tagName })
+          tags.push(newTag)
+        }
+      }
+      blog.tags = tags
+      await queryRunner.manager.getRepository(BlogEntity).save(blog)
+      await queryRunner.commitTransaction()
+    } catch (error) {
+      await queryRunner.rollbackTransaction()
+      throw new BadRequestException(error)
+    } finally {
+      await queryRunner.release()
+    }
+  }
 
   async findBlogsWithPaginationAtHomeDomain(
     pagenationOptions: IPaginationOptions,
